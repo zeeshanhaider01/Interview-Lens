2d4c14bce8be75446fa8bb96116f2ef91566d6f1
zeeshanhaider01 <zeeshanhaider612@gmail.com>
2025-09-18 09:24:45 +0500

    changed @require_Get  to @require_safe decorator in backend/interviewlens/views.py

--- backend/interviewerlens/views.py
+++ backend/interviewerlens/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_safe
 
-@require_GET
+@require_safe
 def health(request):
     return JsonResponse({"status": "ok"})
